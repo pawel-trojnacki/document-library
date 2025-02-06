bfc4485e6c25d012583486f7164ec48b558fa088
Paweł Trojnacki <trojnacki.p@outlook.com>
2025-02-06 22:59:19 +0100

    Add refresh token functionality

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
